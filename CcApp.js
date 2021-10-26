136e71f4a89890e7573c0f83dd4056578851cf2a
Alex Brickwedde <alex_git@brickwedde.de>
2021-10-26 10:18:06 +0200

    Helpers

--- CcApp.js
+++ CcApp.js
@@ -1,3 +1,25 @@
+String.prototype.escapeXml = function escapeXml () {
+  var span = document.createElement("span");
+  span.innerText = this;
+  return span.innerHTML;
+}
+
+const html = function html(strings, ...values) {
+  let str = '';
+  strings.forEach((string, i) => {
+    var s = (values.length > i) ? values[i] : "";
+    if (!isDefined(s) || !s.escapeXml) {
+      if (isDefined(s) && s.toString) {
+        s = s.toString();
+      } else {
+        s = "" + s;
+      }
+    }
+    str += string + s.escapeXml();
+  });
+  return str;
+}
+
 class CcApp extends HTMLElement {
   constructor() {
     super();
